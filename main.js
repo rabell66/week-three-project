19060bde3b117795f27f6c9f51b63b0b08896f83
Robert Abell <robertabell@Roberts-MBP.attlocal.net>
2017-06-10 12:00:48 -0500

    solved operator issues

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 
 var calculate = []
 var num = 0
-var calc = 0
+var calc = ""
+var maxLength = 0
 
 	function showInput(x) {
          num = x;
          
+        
+        
+        if (num >10 && calc == ""){
+             num == 10
+         }
          
-         
-        if (num == 10){
+        else if (num == 10){ 
             document.querySelector(".display").innerHTML = ""
+        calculate=[];
+        num = 0;
+        calc =""
+       
         }
        
 
         else if(num == 11){
               
               
-               calculate.push(calc)
+               calculate.push(Number(calc))
                document.querySelector(".display").innerHTML=document.querySelector(".display").innerHTML+"/";
-               calc = 0;
+               calc = "";
+               num = 0;
                calculate.push("/")
              
               
@@ -26,72 +36,81 @@ var calc = 0
         else if(num == 12){
               
               
-               calculate.push(calc)
+                calculate.push(Number(calc))
                document.querySelector(".display").innerHTML=document.querySelector(".display").innerHTML+"*";
-               calc = 0;
+               calc = "";
+               num = 0;
                calculate.push("*");}
            
               
         else if(num == 13){
               
               
-               calculate.push(calc)
+                calculate.push(Number(calc))
                document.querySelector(".display").innerHTML=document.querySelector(".display").innerHTML+"-";
-               calc = 0;
+                calc = "";
+               num = 0;
                calculate.push("-");}
         else if(num == 14){
           
               
-               calculate.push(calc)
+                calculate.push(Number(calc))
                document.querySelector(".display").innerHTML=document.querySelector(".display").innerHTML+"+";
-               calc = 0;
+                calc = "";
+               num = 0;
                calculate.push("+");}
             
            
-           else if(x == 15){
-               console.log(calculate);
+           else if(num == 15){
+               calculate.push(Number(calc))
                document.querySelector(".display").innerHTML=0;
                equation();
                document.querySelector(".display").innerHTML= z;
                
+               
            }
            
-            else
+           
 
-            {mem = 0
+           
+
+        else    {
             document.querySelector(".display").innerHTML = document.querySelector(".display").innerHTML+num;
-            calc = calc + num;
+            calc = calc + "" + num;
+           
                  }
     
 
     }
 
+
+
          function equation(){
+             
              for (i=1;i<calculate.length;i=i+2){
                  calculate.push(calc);
                  
                if (calculate[i]=="/"){ z = (calculate[i-1])/(calculate[i+1])
-                   console.log(z);
-                   console.log(calculate[i]);
-                   console.log(calculate[i+1]);
-                   console.log(calculate[i-1]);
+                //    calculate.slice(i-1,i+1)
+                //    calculate.push()
+                   
                    calculate = [];}
               else if(calculate[i]=="*"){z = (calculate[i-1])*(calculate[i+1])
-                  console.log(calculate);
-                 console.log(z);               
+                           
                 calculate = [];}
                 else if  (calculate[i]=="-"){ z = (calculate[i-1])-(calculate[i+1])
-                   console.log(z);
+                   
                    calculate = [];}
                 else if  (calculate[i]=="+"){ z = (calculate[i-1])+(calculate[i+1])
-                   console.log(z);
+                   
                    calculate = [];}
 
              
          }
         
          }
- 
+  
+      
 
 
 
